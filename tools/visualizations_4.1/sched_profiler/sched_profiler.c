bd4b261cbdf9fc7f92a05de41fb692b2083aa69e
Tingjia <Tingjia@node0.c220g5.advosuwmadison-pg0.wisc.cloudlab.us>
2022-06-14 15:22:59 -0500

    simplify profile output

--- tools/visualizations_4.1/sched_profiler/sched_profiler.c
+++ tools/visualizations_4.1/sched_profiler/sched_profiler.c
@@ -133,15 +133,6 @@ typedef void (*record_load_change_t)(unsigned long, int);
 extern struct rq *sp_cpu_rq(int cpu);
 extern void set_sp_module_set_nr_running
 	(set_nr_running_t __sp_module_set_nr_running);
-extern void set_sp_module_record_scheduling_event
-	(record_scheduling_event_t __sp_module_record_scheduling_event);
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-extern void set_sp_module_record_scheduling_event_extra
-	(record_scheduling_event_extra_t
-		__sp_module_record_scheduling_event_extra);
-#endif
-extern void set_sp_module_record_balancing_event
-	(record_balancing_event_t __sp_module_record_balancing_event);
 extern void set_sp_module_record_load_change
 	(record_load_change_t __sp_module_record_load_change);
 
@@ -205,18 +196,12 @@ static const struct file_operations rq_stats_fops = {
 
 static void rq_stats_do_work(struct seq_file *m, unsigned long iteration)
 {
-	int i, src_cpu, dst_cpu;
+	int i;
 	sample_entry_t *entry;
 	static unsigned int rq_size[MAX_CPUS] = INITIAL_CPU_ARRAY;
-	static unsigned int last_scheduling_event[MAX_CPUS] = INITIAL_CPU_ARRAY;
 	static unsigned long load[MAX_CPUS] = INITIAL_CPU_ARRAY;
 
 	set_sp_module_set_nr_running(NULL);
-	set_sp_module_record_scheduling_event(NULL);
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-	set_sp_module_record_scheduling_event_extra(NULL);
-#endif
-	set_sp_module_record_balancing_event(NULL);
 	set_sp_module_record_load_change(NULL);
 
 	n_sample_entries = current_sample_entry_id < MAX_SAMPLE_ENTRIES ?
@@ -224,43 +209,9 @@ static void rq_stats_do_work(struct seq_file *m, unsigned long iteration)
 
 	entry = &sample_entries[iteration];
 
-	if (entry->entry_type == BALANCING_SAMPLE)
-	{
-		if (entry->data.balancing_sample.event_type >=
-		    SP_CONSIDERED_CORES_SIS)
-		{
-			seq_printf(m, "# %llu nsecs BA %4u %4u %4u %016llx\n",
-				   entry->sched_clock,
-				   entry->data.balancing_sample.event_type,
-				   entry->data.balancing_sample.cpu,
-				   entry->data.balancing_sample.current_cpu,
-				   entry->data.balancing_sample.data);
-		}
-		else
-		{
-			seq_printf(m, "# %llu nsecs BA %4u %4u\n",
-				   entry->sched_clock,
-				   entry->data.balancing_sample.event_type,
-				   entry->data.balancing_sample.cpu);
-		}
-
-		return;
-	}
 
 	if (entry->entry_type == RQ_SIZE_SAMPLE)
 	{
-		if (rq_size[entry->data.rq_size_sample.dst_cpu] >
-			entry->data.rq_size_sample.nr_running)
-		{
-			/*
-			 * Rq_size has decreased, reset event type, because the
-			 * last event on this core isn't related to the thread
-			 * that's running on it.
-			 */
-			last_scheduling_event
-				[entry->data.rq_size_sample.dst_cpu] = -1;
-		}
-
 		rq_size[entry->data.rq_size_sample.dst_cpu] =
 			entry->data.rq_size_sample.nr_running;
 	}
@@ -269,29 +220,9 @@ static void rq_stats_do_work(struct seq_file *m, unsigned long iteration)
 		load[entry->data.load_sample.cpu] =
 			entry->data.load_sample.load;
 	}
-	else if (entry->entry_type == SCHEDULING_SAMPLE &&
-		 entry->data.scheduling_sample.event_type <
-		 SP_CONSIDERED_CORES_SIS)
-			 /*
-			  * Only record idle balance and periodic rebalancing
-			  * events until the run queue size decreases.
-			  * Otherwise, all of these events will be replaced by
-			  * the wake ups that follow...
-			  */
-			 // && entry->data.scheduling_sample.event_type >= 10)
-	{
-		last_scheduling_event[entry->data.scheduling_sample.dst_cpu] =
-			entry->data.scheduling_sample.event_type;
-	}
 
-#ifdef RQ_SIZES_ONLY
 	for (i = 0; i < num_online_cpus(); i++)
 		seq_printf(m, "%4d", rq_size[i]);
-#else
-	for (i = 0; i < num_online_cpus(); i++)
-		seq_printf(m, "%4d%4d%12ld", rq_size[i],
-			   last_scheduling_event[i], load[i]);
-#endif
 
 	seq_printf(m, "	 %llu nsecs", entry->sched_clock);
 
@@ -305,32 +236,6 @@ static void rq_stats_do_work(struct seq_file *m, unsigned long iteration)
 		seq_printf(m, " LD %4d %12ld", entry->data.load_sample.cpu,
 			   entry->data.load_sample.load);
 	}
-	else if (entry->entry_type == SCHEDULING_SAMPLE)
-	{
-		src_cpu = entry->data.scheduling_sample.src_cpu == 255 ? -1 :
-					  entry->data.scheduling_sample.src_cpu;
-		dst_cpu = entry->data.scheduling_sample.dst_cpu == 255 ? -1 :
-					  entry->data.scheduling_sample.dst_cpu;
-
-		seq_printf(m, " SC %4d %4d %4d",
-			   entry->data.scheduling_sample.event_type,
-			   src_cpu, dst_cpu);
-	}
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-	else if (entry->entry_type == SCHEDULING_SAMPLE_EXTRA)
-	{
-		seq_printf(m, " EX %4u %4d %4d %4d %4d %4d %4d %4d %4d",
-			   entry->data.scheduling_sample_extra.event_type,
-			   entry->data.scheduling_sample_extra.data1,
-			   entry->data.scheduling_sample_extra.data2,
-			   entry->data.scheduling_sample_extra.data3,
-			   entry->data.scheduling_sample_extra.data4,
-			   entry->data.scheduling_sample_extra.data5,
-			   entry->data.scheduling_sample_extra.data6,
-			   entry->data.scheduling_sample_extra.data7,
-			   entry->data.scheduling_sample_extra.data8);
-	}
-#endif
 
 	seq_printf(m, "\n");
 
@@ -356,17 +261,12 @@ void sched_profiler_set_nr_running(int *nr_running_p, int new_nr_running,
 	if (__current_sample_entry_id >= MAX_SAMPLE_ENTRIES)
 	{
 		set_sp_module_set_nr_running(NULL);
-		set_sp_module_record_scheduling_event(NULL);
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-		set_sp_module_record_scheduling_event_extra(NULL);
-#endif
-		set_sp_module_record_balancing_event(NULL);
 		set_sp_module_record_load_change(NULL);
 		return;
 	}
 
 	sample_entries[__current_sample_entry_id]
-		.sched_clock = sched_clock();
+		.sched_clock = ktime_get_mono_fast_ns();
 	sample_entries[__current_sample_entry_id]
 		.entry_type = RQ_SIZE_SAMPLE;
 	sample_entries[__current_sample_entry_id].data.rq_size_sample
@@ -375,121 +275,6 @@ void sched_profiler_set_nr_running(int *nr_running_p, int new_nr_running,
 		.dst_cpu = (unsigned char)dst_cpu;
 }
 
-void sched_profiler_record_scheduling_event(int event_type, int src_cpu,
-					    int dst_cpu)
-{
-	unsigned long __current_sample_entry_id;
-
-	__current_sample_entry_id =
-		__sync_fetch_and_add(&current_sample_entry_id, 1);
-
-	if (__current_sample_entry_id >= MAX_SAMPLE_ENTRIES)
-	{
-		set_sp_module_set_nr_running(NULL);
-		set_sp_module_record_scheduling_event(NULL);
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-		set_sp_module_record_scheduling_event_extra(NULL);
-#endif
-		set_sp_module_record_balancing_event(NULL);
-		set_sp_module_record_load_change(NULL);
-		return;
-
-	}
-
-	sample_entries[__current_sample_entry_id]
-		 .sched_clock = sched_clock();
-	sample_entries[__current_sample_entry_id]
-		 .entry_type = SCHEDULING_SAMPLE;
-	sample_entries[__current_sample_entry_id].data.scheduling_sample
-		 .event_type = (unsigned char)event_type;
-	sample_entries[__current_sample_entry_id].data.scheduling_sample
-		 .src_cpu = (unsigned char)src_cpu;
-	sample_entries[__current_sample_entry_id].data.scheduling_sample
-		 .dst_cpu = (unsigned char)dst_cpu;
-}
-
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-void sched_profiler_record_scheduling_event_extra(int event_type,
-		 char data1, char data2, char data3, char data4,
-		 char data5, char data6, char data7, char data8)
-{
-	unsigned long __current_sample_entry_id;
-
-	__current_sample_entry_id =
-		__sync_fetch_and_add(&current_sample_entry_id, 1);
-
-	if (__current_sample_entry_id >= MAX_SAMPLE_ENTRIES)
-	{
-		set_sp_module_set_nr_running(NULL);
-		set_sp_module_record_scheduling_event(NULL);
-		set_sp_module_record_scheduling_event_extra(NULL);
-		set_sp_module_record_balancing_event(NULL);
-		set_sp_module_record_load_change(NULL);
-		return;
-
-	}
-
-	 sample_entries[__current_sample_entry_id]
-		 .sched_clock = sched_clock();
-	 sample_entries[__current_sample_entry_id]
-		 .entry_type = SCHEDULING_SAMPLE_EXTRA;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .event_type = (unsigned char)event_type;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .data1 = data1;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .data2 = data2;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .data3 = data3;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .data4 = data4;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .data5 = data5;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .data6 = data6;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .data7 = data7;
-	 sample_entries[__current_sample_entry_id].data.scheduling_sample_extra
-		 .data8 = data8;
-}
-#endif
-
-void sched_profiler_record_balancing_event(int event_type, int cpu,
-					   uint64_t data)
-{
-	unsigned long __current_sample_entry_id;
-
-	if (cpu < 0) return;
-
-	__current_sample_entry_id =
-		__sync_fetch_and_add(&current_sample_entry_id, 1);
-
-	if (__current_sample_entry_id >= MAX_SAMPLE_ENTRIES)
-	{
-		set_sp_module_set_nr_running(NULL);
-		set_sp_module_record_scheduling_event(NULL);
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-		set_sp_module_record_scheduling_event_extra(NULL);
-#endif
-		set_sp_module_record_balancing_event(NULL);
-		set_sp_module_record_load_change(NULL);
-		return;
-
-	}
-
-	sample_entries[__current_sample_entry_id]
-		 .sched_clock = sched_clock();
-	sample_entries[__current_sample_entry_id]
-		 .entry_type = BALANCING_SAMPLE;
-	sample_entries[__current_sample_entry_id].data.balancing_sample
-		 .event_type = (unsigned char)event_type;
-	sample_entries[__current_sample_entry_id].data.balancing_sample
-		 .cpu = (unsigned char)cpu;
-	sample_entries[__current_sample_entry_id].data.balancing_sample
-		 .current_cpu = (unsigned char)smp_processor_id();
-	sample_entries[__current_sample_entry_id].data.balancing_sample
-		 .data = data;
-}
 
 void sched_profiler_record_load_change(unsigned long load, int cpu)
 {
@@ -501,11 +286,6 @@ void sched_profiler_record_load_change(unsigned long load, int cpu)
 	if (__current_sample_entry_id >= MAX_SAMPLE_ENTRIES)
 	{
 		set_sp_module_set_nr_running(NULL);
-		set_sp_module_record_scheduling_event(NULL);
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-		set_sp_module_record_scheduling_event_extra(NULL);
-#endif
-		set_sp_module_record_balancing_event(NULL);
 		set_sp_module_record_load_change(NULL);
 		return;
 	}
@@ -514,7 +294,7 @@ void sched_profiler_record_load_change(unsigned long load, int cpu)
 		 .entry_type = LOAD_SAMPLE;
 
 	sample_entries[__current_sample_entry_id]
-		 .sched_clock = sched_clock();
+		 .sched_clock = ktime_get_mono_fast_ns();
 	sample_entries[__current_sample_entry_id]
 		 .entry_type = LOAD_SAMPLE;
 	sample_entries[__current_sample_entry_id].data.load_sample
@@ -540,15 +320,7 @@ int init_module(void)
 	}
 
 	set_sp_module_set_nr_running(sched_profiler_set_nr_running);
-	set_sp_module_record_scheduling_event
-		(sched_profiler_record_scheduling_event);
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-	set_sp_module_record_scheduling_event_extra
-		(sched_profiler_record_scheduling_event_extra);
-#endif
-	set_sp_module_record_balancing_event
-		(sched_profiler_record_balancing_event);
-	set_sp_module_record_load_change(sched_profiler_record_load_change);
+	set_sp_module_record_load_change(NULL);
 
 	return 0;
 }
@@ -556,11 +328,6 @@ int init_module(void)
 void cleanup_module(void)
 {
 	set_sp_module_set_nr_running(NULL);
-	set_sp_module_record_scheduling_event(NULL);
-#ifdef WITH_SCHEDULING_SAMPLE_EXTRA
-	set_sp_module_record_scheduling_event_extra(NULL);
-#endif
-	set_sp_module_record_balancing_event(NULL);
 	set_sp_module_record_load_change(NULL);
 
 	ssleep(1);
